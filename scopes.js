e5bf1ac6c3f8c499e60e535fe60fb5456b97ad28
beyondsense-lab <officialbeyondsense@gmail.com>
2026-09-15 22:05:11 +0530

    Update scopes.js

--- scopes.js
+++ scopes.js
@@ -3,3 +3,4 @@ function fun(){
   var teacher = "LAURA"; //function scoped variable teacher
   console.log ("hello", teacher); //will print function scoped teacher as the active scope is function fun
 }
+fun();
