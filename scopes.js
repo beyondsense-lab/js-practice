9ae1cc69079791a01227cc9c7ca9308eac8b0bb7
beyondsense-lab <officialbeyondsense@gmail.com>
2026-09-15 22:04:51 +0530

    Create scopes.js

---
+++ scopes.js
@@ -0,0 +1,5 @@
+var teacher = "Aditya"; //global scoped variable teacher
+function fun(){
+  var teacher = "LAURA"; //function scoped variable teacher
+  console.log ("hello", teacher); //will print function scoped teacher as the active scope is function fun
+}
